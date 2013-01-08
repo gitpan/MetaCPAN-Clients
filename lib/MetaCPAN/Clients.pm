5b995a8ee4e734234da039717e4e8b484ad48def
גאבור סבו - Gábor Szabó <gabor@pti.co.il>
2013-01-08 10:30:19 +0000

    Import of SZABGAB/MetaCPAN-Clients-0.01 from CPAN.
    gitpan-cpan-distribution: MetaCPAN-Clients
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SZABGAB/MetaCPAN-Clients-0.01.tar.gz
    gitpan-cpan-author:       SZABGAB
    gitpan-cpan-maturity:     released

---
+++ lib/MetaCPAN/Clients.pm
@@ -0,0 +1,16 @@
+package MetaCPAN::Clients;
+{
+  $MetaCPAN::Clients::VERSION = '0.01';
+}
+use strict;
+use warnings;
+
+=head1 DESCRIPTION
+
+For an explanation see L<Fetching META data from Meta CPAN|http://perl5maven.com/fetching-meta-data-from-meta-cpan>.
+
+=cut
+
+
+1;
+
